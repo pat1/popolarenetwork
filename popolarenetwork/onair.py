c77a148cda964f0f50c98c63be7e28f8cf4a1425
Paolo Patruno <p.patruno@iperbole.bologna.it>
2025-07-24 21:52:43 +0200

    working on onair

--- popolarenetwork/onair.py
+++ popolarenetwork/onair.py
@@ -4,51 +4,98 @@ from popolarenetwork import jsonrpc
 import logging
 import time
 import datetime
-from autoradio.gest_program import gest_program
-        
+from autoradio.gest_palimpsest import gest_palimpsest
+from threading import *
+
+SCHEDULE_MINUTES=120
+SCHEDULE_HALF_MINUTES=SCHEDULE_MINUTES/2
+SCHEDULE_SECONDS=SCHEDULE_MINUTES*60
+SCHEDULE_SHIFT_SECONDS=SCHEDULE_SECONDS/3
+
+def onair(client,status):
+        # call a remote-procedure over serial transport
+        result = client.onair(status=status)
+        logging.info(f"jsonrpc onair {status}; result: {result}")
+
 def main(timestampfile="record.timestamp",jsonrpcfile=None):
 
     logging.info("start")
     
-# create JSON-RPC client
-
+    # create JSON-RPC client
     if (jsonrpcfile is None):
         logfunc=jsonrpc.log_dummy
     else:
         logfunc=jsonrpc.log_file(jsonrpcfile)
     
     client = jsonrpc.ServerProxy(jsonrpc.JsonRpc20(radio=False,notification=False),
-                             jsonrpc.TransportSERIAL( logfunc=logfunc,
-                                                      port='/dev/popolare_onair',baudrate=115200,timeout=5))
+                        jsonrpc.TransportSERIAL( logfunc=logfunc,
+                        port='/dev/popolare_onair',baudrate=115200,timeout=5))
 
+    onair(client,False)
+
+    # this is the first and last time that I set now with the current time
+    scheduletimedelta=datetime.timedelta(minutes=SCHEDULE_MINUTES)
+    shiftscheduletimedelta=datetime.timedelta(seconds=SCHEDULE_SHIFT_SECONDS)
+    datetimeelab=datetime.datetime.now()+shiftscheduletimedelta
+    
     try:
-        # call a remote-procedure (with positional parameters)
         while (True):
+            #select the programs
+            logging.info(f"datetimeelab: {datetimeelab}")
+            pro=gest_palimpsest(datetimeelab,SCHEDULE_HALF_MINUTES)
 
+            timers=[]
+            # do a list
+            for program in pro.get_program():
+
+                code=program.show.type.code
+                if (code =="1a"):
+                    length=program.show.length
+                    if length is None:
+                        logging.warning("get_palimpsest: %s legth is None; setting default to 900 sec",str(program))
+                        length = 900
+                    pdatetime_start=program.ar_scheduledatetime
+                    title=str(program)
+                    pdatetime_end=program.ar_scheduledatetime+datetime.timedelta(seconds=length)
+                    type=program.show.type.type
+                    subtype=program.show.type.subtype
+                    production=program.show.production
+                    note=program.show.description
+
+                    logging.info(f"code: {code}, type: {type}, subtype: {subtype}; title: {title}, start: {pdatetime_start}, end: {pdatetime_end}")
+                    now=datetime.datetime.now()
+                    status = True
+                    delay=max((pdatetime_start-now).seconds,0)
+                    t = Timer(delay, onair,[client,status])
+                    logging.info(f"timer start {delay}")
+                    t.start()
+                    timers.append(t)
+
+                    now=datetime.datetime.now()
+                    status = False
+                    delay=max((pdatetime_end-now).seconds,0)
+                    t = Timer(delay, onair,[client,status])
+                    logging.info(f"timer stop  {delay}")
+                    timers.append(t)
+                    
             # time constants
             now=datetime.datetime.now()
-
-            #select the programs
-            pro=gest_program(now,60)
-
-            # do a list
-            for programma in pro.get_program():
-        
-                print(programma.ar_filename)
-                print(programma.ar_url)
-                print(programma.ar_scheduledatetime)
-                print(programma.ar_length)
-
-            result = client.onair(status=True)
-            print (result)
-            time.sleep(3)
-            result = client.onair(status=False)
-            print (result)
-            time.sleep(3)
+            datetimeelab=datetimeelab+scheduletimedelta
+            delay=(datetimeelab-shiftscheduletimedelta)-now
+            logging.info(f"delay for {delay}")
+            time.sleep(delay.seconds)
     
     except KeyboardInterrupt:
     #except:
         logging.info("terminate process")
+        try:
+            for t in timers:
+                t.cancel()
+        except:
+            pass
+
+        onair(client,False)
+
 
 if __name__ == "__main__":
     main()
